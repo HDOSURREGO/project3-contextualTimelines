e20fba9ed9caf0d774ae229324858681abbca93c
Henry Hoyos <hdosurrego@gmail.com>
2019-11-09 16:32:38 -0500

    first heroku commit

---
+++ public/precache-manifest.c30b580809feaa703073b0204ee45b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c086246ed16ca71c0af40c505786de5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "239777c67d59947b92ac",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "58385c44c0d12e062971",
+    "url": "/static/css/main.90a4f3ef.chunk.css"
+  },
+  {
+    "revision": "239777c67d59947b92ac",
+    "url": "/static/js/2.e3a749bb.chunk.js"
+  },
+  {
+    "revision": "58385c44c0d12e062971",
+    "url": "/static/js/main.5fc10907.chunk.js"
+  },
+  {
+    "revision": "30f7e88dfb8dc58a6917",
+    "url": "/static/js/runtime-main.599c1c84.js"
+  }
+]);
