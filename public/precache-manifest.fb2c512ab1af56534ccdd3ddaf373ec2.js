1a2ab15eb35bb5d7c792ff7d99a6d82a9fa0cb4c
Henry Hoyos <hdosurrego@gmail.com>
2020-02-26 22:11:37 -0500

    Commit to heroku new home.js

---
+++ public/precache-manifest.fb2c512ab1af56534ccdd3ddaf373ec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1069ca4c5372e73b85a506a3a4c8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba5533fd03b9befbdbf3",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d74471a579863446608b",
+    "url": "/static/css/main.7ba90e20.chunk.css"
+  },
+  {
+    "revision": "ba5533fd03b9befbdbf3",
+    "url": "/static/js/2.531ed4f1.chunk.js"
+  },
+  {
+    "revision": "d74471a579863446608b",
+    "url": "/static/js/main.7fedd598.chunk.js"
+  },
+  {
+    "revision": "30f7e88dfb8dc58a6917",
+    "url": "/static/js/runtime-main.599c1c84.js"
+  },
+  {
+    "revision": "946e37756498fccf177ae85d9b8c1d0f",
+    "url": "/static/media/background.946e3775.jpg"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ed3f672819dd96acf5cbb8f27585b921.js"
+  "/precache-manifest.fb2c512ab1af56534ccdd3ddaf373ec2.js"
 );
 
 self.addEventListener('message', (event) => {
